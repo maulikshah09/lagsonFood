a14758894c7a7097dc5991fe731a453e304a6946
Maulik shah <maulik_shah@hotmail.co.in>
2025-01-02 19:02:15 +0530

    Maulik

--- lasagnaCook/ViewController.swift
+++ lasagnaCook/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+ 
+        let lasagna = Lasagna()
+
+        let elapsedTime = 25
+        let remainingTime = lasagna.remainingMinutesInOven(elapsedMinutes: elapsedTime)
+        print("Remaining minutes in oven: \(remainingTime)")
+
+        let layers = 3
+        let prepTime = lasagna.preparationTimeInMinutes(layers: layers)
+        print("Preparation time for \(layers) layers: \(prepTime) minutes")
+
+        let totalTime = lasagna.totalTimeInMinutes(layers: layers, elapsedMinutes: elapsedTime)
+        print("Total time (preparation + elapsed cooking): \(totalTime) minutes")
     }
 
 
